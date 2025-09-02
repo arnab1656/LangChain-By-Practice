95e95ca018997020de92e5755cc5973570b0f420
arnab1656 <arnab.paul.1656@gmail.com>
2025-09-02 21:05:25 +0530

    Enhanced API call

--- Langchain-Models/1_LLM/openAi_Call.py
+++ Langchain-Models/1_LLM/openAi_Call.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv() 
-openai_api_key = os.getenv("OPEN_API_KEY")
 
+# Debugg
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# print("Key is ->",openai_api_key)
 
-llm = OpenAI(model = "gpt-3.5-turbo-instruct",openai_api_key = openai_api_key)
+llm = OpenAI(model = "gpt-3.5-turbo-instruct")
 
 result  = llm.invoke("Whats the capital of India")
 
