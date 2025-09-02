87fc988d6f22231f23e2456af2cdebe90b48cad9
arnab1656 <arnab.paul.1656@gmail.com>
2025-09-02 20:56:40 +0530

    OPEN_AI implementation

--- Langchain-Models/1_LLM/openAi_Call.py
+++ Langchain-Models/1_LLM/openAi_Call.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv() 
-
-llm = OpenAI(model = "gpt-3.5-turbo-instruct")
-
-llm.invoke("Whats the capital of India")
+openai_api_key = os.getenv("OPEN_API_KEY")
 
 
+llm = OpenAI(model = "gpt-3.5-turbo-instruct",openai_api_key = openai_api_key)
+
+result  = llm.invoke("Whats the capital of India")
+
+print(result)
